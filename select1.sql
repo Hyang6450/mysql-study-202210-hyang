e6a7ee50f92249534338ea0c0a4c910888ba56f1
Hyang6450 <s1422@naver.com>
2022-12-14 21:47:40 +0900

    select

---
+++ select1.sql
@@ -0,0 +1,32 @@
+select 
+	*
+from 
+	student_study_mst
+where 
+	age between 25 and 30;
+    
+select 
+	*
+from 
+	student_study_mst
+where 
+	age not between 25 and 30;
+    
+select 
+	*
+from 
+	student_study_mst
+where 
+	not age = 25;
+
+select 
+	*
+from 
+	student_study_mst
+where 
+	age is null;
+    
+    
+    
+
+
