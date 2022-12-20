c1bc149b2d032eadf280250bc088c20787d5c596
Hyang6450 <s1422@naver.com>
2022-12-20 14:16:13 +0900

    실습

--- 실습.sql
+++ 실습.sql
@@ -19,7 +19,57 @@ from
 select
 	*
 from 
-	school_mst sm
-	left outer join student_mst stm on (stm.school_id = sm.school_id)
+	student_mst stm 
+	left outer join school_mst sm on (sm.school_id = stm.school_id)
 where
 	sm.school_name like '서울대%';
+
+select
+	*
+from 
+	school_mst 
+where
+	school_id in (select 
+					school_id
+				  from
+					school_mst
+				  where
+                    school_name like '서울대%'
+			     or school_name like '부산대%');
+	
+    
+select 
+	*
+from 	
+	student_mst
+where
+	school_id in (1, 2); /*in (1,2)안에 값이 하나라도 포함되어 있으면 True*/
+    
+select
+	*,
+    (select school_name from school_mst scm where scm.school_id = stm.school_id) as 번호
+from
+	student_mst stm;
+
+set profiling = 1;
+show profiles;
+
+select 
+	count(*) as 총인원
+from
+	student_mst;
+    
+select 
+	*,
+    (select 
+		count(*) 
+	from 
+		student_mst) as 총인원
+from 
+	student_mst;
+    
+select 
+	*
+from 
+	student_mst sm
+	left outer join	(select count(*) as 총인원 from student_mst) sc on (1 = 1);
